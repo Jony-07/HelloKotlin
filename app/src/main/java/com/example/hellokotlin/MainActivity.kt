3401538f5846ab041eab1a31e354c3752db8ef06
Jony-07 <jony25lopezml@gmail.com>
2023-02-13 16:20:46 -0600

    End of first example

--- app/src/main/java/com/example/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.hellokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textViewStart = findViewById<TextView>(R.id.txtStart)
+
+        findViewById<Button>(R.id.btnReeady).setOnClickListener {
+            textViewStart.visibility = View.VISIBLE
+
+            findViewById<TextView>(R.id.txtHello).text = "You can do it"
+        }
     }
 }
